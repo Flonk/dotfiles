cb11bd0071beed90a26d2a5fedacd82c77b6c0f4
Flonk <schindlabua@gmail.com>
2023-10-16 09:19:42 +0200

    aaa

--- private_dot_config/i3/executable_polybar.sh
+++
@@ -1,9 +0,0 @@
-killall polybar
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload i3 -c ~/.config/polybar/config.ini &
-  done
-else
-  polybar --reload i3 -c ~/.config/polybar/config.ini &
-fi
