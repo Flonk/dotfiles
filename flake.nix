6f29eac3e2eb16103d13095eef26a604d7e628a2
Florian Schindler <schindlabua@gmail.com>
2025-05-24 14:28:39 +0200

    setup flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager-25.05";
-  };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      schnitzelwirt = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/schnitzelwirt.nix
-        ];
-      };
-    };
-
-    homeConfigurations = {
-      flo = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home-manager/flo.nix
-        ];
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations = {
+        schnitzelwirt = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./hosts/schnitzelwirt.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        flo = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home-manager/flo.nix
+          ];
+        };
+      };
+    };
+  
 }
