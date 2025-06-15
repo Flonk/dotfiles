baf1ecc9e45cd19c12e701e8432e487f64584556
Flonk <schindlabua@gmail.com>
2025-06-15 16:33:19 +0200

    asdas

--- home/modules/vscode/vscode.nix
+++ home/modules/vscode/vscode.nix
@@ -9,29 +9,6 @@
   
   programs.vscode = {
     enable = true;
-    mutableExtensionsDir = false;
-    
-    profiles.default.extensions = with pkgs.vscode-marketplace; [
-      alexanderte.dainty-nord-vscode
-      astro-build.astro-vscode
-      bbenoist.nix
-      brettm12345.nixfmt-vscode
-      dandric.vscode-jq
-      dbaeumer.vscode-eslint
-      esbenp.prettier-vscode
-      github.copilot
-      github.copilot-chat
-      hashicorp.terraform
-      iliazeus.vscode-ansi
-      jnoortheen.nix-ide
-      mechatroner.rainbow-csv
-      ms-azuretools.vscode-azureresourcegroups
-      ms-azuretools.vscode-azurestorage
-      ms-azuretools.vscode-cosmosdb
-      ms-vscode.hexeditor
-      oderwat.indent-rainbow
-      unifiedjs.vscode-mdx
-    ];
   };
   
 }
