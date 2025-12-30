0bdb280734915665aea689f2d5459d662b0bb258
Florian Schindler <florian.schindler@andamp.io>
2025-12-30 14:39:26 +0100

    THE TWO MOST IMPORTANT THINGS JIRA AND MINECRAFT

--- home/modules/minecraft/minecraft.nix
+++ home/modules/minecraft/minecraft.nix
@@ -8,13 +8,17 @@
 
   home.packages = with pkgs; [
     prismlauncher
-    # Fabric installer for Minecraft modding
-    (pkgs.writeShellScriptBin "fabric-installer" ''
-      exec ${pkgs.jre}/bin/java -jar ${pkgs.fetchurl {
-        url = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.1.1/fabric-installer-1.1.1.jar";
-        sha256 = "0y8vwcp1sn0dh77nn3k2vhpbcsn2fwni99v5a9hc5ngrssfsd1r4";
-      }} "$@"
-    '')
   ];
+
+  xdg.desktopEntries = {
+    prismlauncher = {
+      name = "Prism Launcher";
+      comment = "A custom launcher for Minecraft";
+      exec = "prismlauncher";
+      icon = "prismlauncher";
+      categories = [ "Game" ];
+      terminal = false;
+    };
+  };
 
 }
