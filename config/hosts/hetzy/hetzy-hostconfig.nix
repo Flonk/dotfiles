100408d60431a4b33ef823a23e0719d112a1bf4d
Florian Schindler <florian.schindler@andamp.io>
2026-03-11 21:24:22 +0100

    HETZY ^_^

--- config/hosts/hetzy/hetzy-hostconfig.nix
+++ config/hosts/hetzy/hetzy-hostconfig.nix
@@ -3,7 +3,7 @@
   config.skynet = {
     host = {
       adminUser = "zeroclaw";
-      motd.command = "printf '%b' '\\033[38;5;208m╻ ╻┏━╸╺┳╸╺━┓╻ ╻\\n┣━┫┣╸  ┃ ┏━┛┗┳┛\\n╹ ╹┗━╸ ╹ ┗━╸ ╹\\033[94m    A Linux VPS.\\n                    Built with NixOS and Skynet.\\033[0m\\n'";
+      motd.command = "printf '%b' '\\033[38;5;208m╻ ╻┏━╸╺┳╸╺━┓╻ ╻\\n┣━┫┣╸  ┃ ┏━┛┗┳┛\\n╹ ╹┗━╸ ╹ ┗━╸ ╹\\033[94m    Built with NixOS and Skynet.\\033[0m\\n'";
       ssh.authorizedKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0I6XBx5UT8ZnxEcfgdBZgy+1ub4QDrtLlgrpVIw//0 florian.schindler@andamp.io"
       ];
