ca8010671a0f915c2ee4f465db055bc236ad982f
Flonk <schindlabua@gmail.com>
2026-04-23 17:07:52 +0200

    idk

--- modules/development/obsidian/home.nix
+++ modules/development/obsidian/home.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf config.skynet.module.development.obsidian.enable {
     home.packages = with pkgs; [ obsidian ];
 
+    stylix.targets.obsidian.enable = true;
     stylix.targets.obsidian.vaultNames = lib.mkIf config.skynet.module.desktop.stylix.enable [
       "Vault"
     ];
